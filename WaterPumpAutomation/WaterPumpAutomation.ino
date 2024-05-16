2a3c9e1a3d851f319b3aabec299d521c3aca8e25
Dieter Vanrykel <dieter.vanrykel@pxl.be>
2024-05-16 16:36:11 +0200

    added TimerOn-Delay

--- WaterPumpAutomation/WaterPumpAutomation.ino
+++ WaterPumpAutomation/WaterPumpAutomation.ino
@@ -4,6 +4,10 @@ const int waterSensor = 7;    // Pin to detect water
 const int relayPin1 = 8;      // Pin for relay Live wire
 const int relayPin2 = 9;      // Pin for relay Neutral wire
 
+bool pumpActive = true;
+
+unsigned long startTime = 0;
+
 void setup() {
   pinMode(relayPin1, OUTPUT); // Live wire pin
   pinMode(relayPin2, OUTPUT); // Neutral wire pin
@@ -20,19 +24,33 @@ void loop() {
   int waterDetected = digitalRead(waterSensor);
   
   if (waterDetected == LOW) {
-    #ifdef DEBUG_SERIAL_OUTPUT
-      Serial.println("Water detected!");
-    #endif
-    // Activate both relays 
-    digitalWrite(relayPin1, LOW);
-    digitalWrite(relayPin2, LOW);
-    delay(8000);                  // Pump 8 seconds     
+    
+    if (!pumpActive) {
+      #ifdef DEBUG_SERIAL_OUTPUT
+        Serial.println("Water detected");
+      #endif
+      startTime = millis();
+      pumpActive = true;
+    }
+    
+    if (pumpActive && (millis() - startTime) > 2000) {
+      #ifdef DEBUG_SERIAL_OUTPUT
+        Serial.println("Pump activated");
+      #endif
+      // Activate both relays 
+      digitalWrite(relayPin1, LOW);
+      digitalWrite(relayPin2, LOW);
+      delay(10000);                  // Pump 10 seconds
+    }     
   } 
   else
   {
     #ifdef DEBUG_SERIAL_OUTPUT
-      Serial.println("No water detected!");
+      if (pumpActive)
+        Serial.println("No water detected!");
     #endif   
+
+    pumpActive = false;
 
     // Deactivate both relays
     digitalWrite(relayPin1, HIGH);
